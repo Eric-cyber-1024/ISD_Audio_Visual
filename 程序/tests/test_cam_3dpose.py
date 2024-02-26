5f7d6468b2397687726ee715959f562c2c0927bd
Brian Leung <swleungbrian@hotmail.com>
2024-02-26 15:34:36 +0800

    revised test_cam_3dpose.py to fix cchessboard with 38mm squares

--- 程序/tests/test_cam_3dpose.py
+++ 程序/tests/test_cam_3dpose.py
@@ -46,21 +46,35 @@ def drawCube(img, corners, imgpts):
 
 if __name__ == '__main__':
 
+    n_row=6 
+    n_col=9
+    square_size = 38 # 38 mm 
+
+    # Define the font settings
+    font = cv.FONT_HERSHEY_SIMPLEX
+    font_scale = 1.
+    font_color = (255, 255, 255)  # White color
+    line_type = cv.LINE_AA
+
+    # Write the debug message on the image
+    debug_message = "Debug Message"
+    text_size, _ = cv.getTextSize(debug_message, font, font_scale, 1)
+    text_x = 10
+    text_y = 10 + text_size[1]
+
+
     # Load previously saved data
     #with np.load('B.npz') as X:
     #    mtx, dist, _, _ = [X[i] for i in ('arr_0.npy', 'arr_1.npy', 'arr_2.npy', 'arr_3.npy')]
     mtx,dist = loadCalibrationData('20240226_1255_1m6_calibration_err0_05.yaml')
 
-    print(mtx)
-    print(dist)
-
     criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
     objp = np.zeros((6*9, 3), np.float32)
-    objp[:, :2] = np.mgrid[0:6, 0:9].T.reshape(-1, 2)
+    objp[:, :2] = np.mgrid[0:6, 0:9].T.reshape(-1, 2)*square_size
 
-    axis = np.float32([[3, 0, 0], [0, 3, 0], [0, 0, -3]]).reshape(-1, 3)
-    axisCube = np.float32([[0, 0, 0], [0, 3, 0], [3, 3, 0], [3, 0, 0],
-                           [0, 0, -3], [0, 3, -3], [3, 3, -3], [3, 0, -3]])
+    axis = np.float32([[1, 0, 0], [0, 1, 0], [0, 0, -1]]).reshape(-1, 3)*square_size
+    axisCube = np.float32([[0, 0, 0], [0, 1, 0], [1, 1, 0], [1, 0, 0],
+                           [0, 0, -1], [0, 1, -1], [1, 1, -1], [1, 0, -1]])*square_size
 
     camera = cv.VideoCapture(1) #Supposed to be the only camera
         
@@ -76,20 +90,30 @@ if __name__ == '__main__':
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         ret, corners = cv.findChessboardCorners(gray, (6, 9), None)
         
-        if ret == True:
+        if ret:
             corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+
+            # Draw and display the corners
+            imgAugmnt = cv.drawChessboardCorners(img, (n_row,n_col), corners2,ret)
+            cv.imshow('Pose Estimation',imgAugmnt) 
 
             # Find the rotation and translation vectors
             ret, rvecs, tvecs = cv.solvePnP(objp, corners2, mtx, dist)
 
-            # project 3D points to image plane
-            imgpts, jac = cv.projectPoints(axis, rvecs, tvecs, mtx, dist)
+            if ret:
+                cv.putText(img, 'solved!!', (text_x, text_y), font, font_scale, font_color, 1, line_type)
+            
 
-            img = draw(img, corners2, imgpts)
-            cv.imshow('Pose Estimation', img)
-            imgptsCube, jacCube = cv.projectPoints(axisCube, rvecs, tvecs, mtx, dist)
-            imgCube = drawCube(img, corners2, imgptsCube)
-            cv.imshow('Pose Estimation Cube', imgCube)
+                # project 3D points to image plane
+                imgpts, jac = cv.projectPoints(axis, rvecs, tvecs, mtx, dist)
+
+                img = draw(img, corners2, imgpts)
+                cv.imshow('Pose Estimation', img)
+                imgptsCube, jacCube = cv.projectPoints(axisCube, rvecs, tvecs, mtx, dist)
+                imgCube = drawCube(img, corners2, imgptsCube)
+                cv.imshow('Pose Estimation Cube', imgCube)
+            
+            
             k = cv.waitKey(50) & 0xff
             if k == 'c':
                 continue
@@ -106,4 +130,4 @@ if __name__ == '__main__':
                 camera.release()        
                 break
 
-    cv.destroyWindow()
+    cv.destroyAllWindow()
