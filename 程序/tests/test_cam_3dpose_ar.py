7df5b78102b5ae7113bfdffaf9346be075d88969
Brian Leung <swleungbrian@hotmail.com>
2024-02-29 09:59:11 +0800

    revised test_cam_3dpose_ar.py, added thread to run depth cam stuffs

--- 程序/tests/test_cam_3dpose_ar.py
+++ 程序/tests/test_cam_3dpose_ar.py
@@ -28,6 +28,228 @@ from roypy_sample_utils import CameraOpener, add_camera_opener_options
 from roypy_platform_utils import PlatformHelper
 
 
+# Add[support for pmd flexx2 depth camera],Brian,29 Feb 2024
+K=3
+mouseX = 0
+mouseY = 0
+
+
+def draw_circle(event,x,y,flags,param):
+    global mouseX,mouseY
+    if event == cv2.EVENT_LBUTTONDOWN:
+        mouseX = x
+        mouseY = y
+    
+
+
+class MyListener(roypy.IDepthDataListener):
+    def __init__(self, q):
+        super(MyListener, self).__init__()
+        self.frame = 0
+        self.done = False
+        self.undistortImage = False
+        self.lock = threading.Lock()
+        self.once = False
+        self.queue = q
+
+    def get3DCoordinates(self,u,v,z):
+        M = self.cameraMatrix
+        fx= M[0][0]
+        fy= M[1][1]
+        cx= M[0][2]
+        cy= M[1][2]
+        x = (u-cx)*(z/fx)
+        y = (v-cy)*(z/fy)
+
+        print(x,y,z)
+
+    
+
+    def onNewData(self, data):
+        p = data.npoints()
+        self.queue.put(p)
+        
+
+    def paint (self, data):
+        """Called in the main thread, with data containing one of the items that was added to the
+        queue in onNewData.
+        """
+        # mutex to lock out changes to the distortion while drawing
+        self.lock.acquire()
+
+        depth = data[:, :, 2]
+        gray = data[:, :, 3]
+        confidence = data[:, :, 4]
+
+        #print(depth[172//2][224//2])
+        z = depth[mouseY//K][mouseX//K]
+        self.get3DCoordinates(mouseX//K,mouseY//K,z)
+        
+        
+
+
+        # image size = 172 x 224 
+        zImage = np.zeros(depth.shape, np.float32)
+        grayImage = np.zeros(depth.shape, np.float32)
+
+        #print(zImage.shape,grayImage.shape)
+
+
+        # iterate over matrix, set zImage values to z values of data
+        # also set grayImage adjusted gray values
+        xVal = 0
+        yVal = 0
+        for x in zImage:        
+            for y in x:
+                if confidence[xVal][yVal]> 0:
+                  zImage[xVal,yVal] = self.adjustZValue(depth[xVal][yVal])
+                  grayImage[xVal,yVal] = self.adjustGrayValue(gray[xVal][yVal])
+                yVal=yVal+1
+            yVal = 0
+            xVal = xVal+1
+
+        zImage8 = np.uint8(zImage)
+        grayImage8 = np.uint8(grayImage)
+
+        # apply undistortion
+        if self.undistortImage:
+            zImage8 = cv2.undistort(zImage8,self.cameraMatrix,self.distortionCoefficients)
+            grayImage8 = cv2.undistort(grayImage8,self.cameraMatrix,self.distortionCoefficients)
+
+        # equalize grayImage8
+        grayImage8 = cv2.equalizeHist(grayImage8)    
+
+        # finally show the images
+        
+        # turn zImage8,grayImage8 to color first
+        zImg = cv2.cvtColor(cv2.resize(zImage8,[zImage8.shape[1]*K,zImage8.shape[0]*K]),cv2.COLOR_GRAY2BGR)
+        gImg = cv2.cvtColor(cv2.resize(grayImage8,[grayImage8.shape[1]*K,grayImage8.shape[0]*K]),cv2.COLOR_GRAY2BGR)
+        
+        # marker mouseX, mouseY on zImg,gImg
+        cv2.circle(zImg,(mouseX,mouseY),1,(0,0,255))
+        cv2.circle(gImg,(mouseX,mouseY),1,(0,0,255))
+
+        cv2.imshow('Depth',zImg)
+        cv2.imshow('Gray', gImg)
+        cv2.setMouseCallback('Gray',draw_circle)
+        
+
+        
+
+        self.lock.release()
+        self.done = True
+
+    def setLensParameters(self, lensParameters):
+        # Construct the camera matrix
+        # (fx   0    cx)
+        # (0    fy   cy)
+        # (0    0    1 )
+        self.cameraMatrix = np.zeros((3,3),np.float32)
+        self.cameraMatrix[0,0] = lensParameters['fx']
+        self.cameraMatrix[0,2] = lensParameters['cx']
+        self.cameraMatrix[1,1] = lensParameters['fy']
+        self.cameraMatrix[1,2] = lensParameters['cy']
+        self.cameraMatrix[2,2] = 1
+
+        print(self.cameraMatrix)
+
+        # Construct the distortion coefficients
+        # k1 k2 p1 p2 k3
+        self.distortionCoefficients = np.zeros((1,5),np.float32)
+        self.distortionCoefficients[0,0] = lensParameters['k1']
+        self.distortionCoefficients[0,1] = lensParameters['k2']
+        self.distortionCoefficients[0,2] = lensParameters['p1']
+        self.distortionCoefficients[0,3] = lensParameters['p2']
+        self.distortionCoefficients[0,4] = lensParameters['k3']
+
+        print(self.distortionCoefficients)
+
+    def toggleUndistort(self):
+        self.lock.acquire()
+        self.undistortImage = not self.undistortImage
+        self.lock.release()
+
+    # Map the depth values from the camera to 0..255
+    def adjustZValue(self,zValue):
+        clampedDist = min(7.9,zValue)
+        newZValue = clampedDist / 7.9 * 255
+        return newZValue
+
+    # Map the gray values from the camera to 0..255
+    def adjustGrayValue(self,grayValue):
+        clampedVal = min(600,grayValue)
+        newGrayValue = clampedVal / 600 * 255
+        return newGrayValue
+
+def process_event_queue (q, painter):
+
+    while True:
+        try:
+            
+            # try to retrieve an item from the queue.
+            # this will block until an item can be retrieved
+            # or the timeout of 1 second is hit
+            if len(q.queue) == 0:
+                item = q.get(True, 1)
+            else:
+                for i in range (0, len (q.queue)):
+                    item = q.get(True, 1)
+        except queue.Empty:
+            # this will be thrown when the timeout is hit
+            break
+        else:
+            painter.paint(item)
+            # waitKey is required to use imshow, we wait for 1 millisecond
+            currentKey = cv2.waitKey(1)
+            if currentKey == ord('d'):
+                painter.toggleUndistort()
+            # close if escape key pressed
+            if currentKey == 27: 
+                break
+
+# depth camera main
+def depthcam_main():
+
+    # Set the available arguments
+    platformhelper = PlatformHelper()
+    parser = argparse.ArgumentParser (usage = __doc__)
+    add_camera_opener_options (parser)
+    options = parser.parse_args()
+   
+    opener = CameraOpener (options)
+
+    try:
+        cam = opener.open_camera ()
+    except:
+        print("could not open Camera Interface")
+        sys.exit(1)
+
+    try:
+        # retrieve the interface that is available for recordings
+        replay = cam.asReplay()
+        print ("Using a recording")
+        print ("Framecount : ", replay.frameCount())
+        print ("File version : ", replay.getFileVersion())
+    except SystemError:
+        print ("Using a live camera")
+
+    q = queue.Queue()
+    l = MyListener(q)
+    cam.registerDataListener(l)
+    cam.startCapture()
+
+    lensP = cam.getLensParameters()
+    l.setLensParameters(lensP)
+
+    process_event_queue (q, l)
+
+    cam.stopCapture()
+    print("Done")
+
+
+    
+
+
 # Add[for getting moving averages],Brian,29 Feb 2024
 class MovingAverageCalculator:
     def __init__(self, nSamples):
@@ -207,6 +429,12 @@ if __name__ == '__main__':
     
     # load calibration data
     k,d = loadCalibrationData('20240226_1255_1m6_calibration_err0_05.yaml')
+
+
+    # run depth camera 
+    depthcam_thread = threading.Thread(target=depthcam_main, daemon=True)
+    depthcam_thread.start()
+
 
     video = cv2.VideoCapture(1)
     time.sleep(2.0)
